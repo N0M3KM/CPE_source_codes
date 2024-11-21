2fc5b28bb4f2ed0f9fdaa9bacc2fc98e8e0e8be2
N0M3KM <trybrybetterthanu@gmail.com>
2024-11-21 19:53:02 +0700

    Add in class 2 lab

---
+++ CPE100/In-Class/In-Class-2/Problem1_1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int isAllDigit(char arr[]) {
+    int i = 0;
+    while (arr[i]!='\0') {
+        if ((arr[i] < '0' || arr[i] > '9')) {
+            return 0;
+        }
+        i++;
+    }
+    return 1;
+}
+
+int main() {
+    char arr[101] = "1115151";
+    if (isAllDigit(arr)) {
+        printf("Is all digit\n");
+    } else {
+        printf("Is not all digit\n");
+    }
+    return 0;
+}
